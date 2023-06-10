c9ecd61e2709e97cb295acd201b24266e2bd3982
Kevin Allbritten <kevinallbritten@gmail.com>
2023-06-10 01:57:21 -0500

    Client side login & registration

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,7 +14,7 @@ export default observer(function ActivityDashboard() {
         if (activityRegistry.size <= 1) loadActivites();
     }, [activityRegistry.size, loadActivites])
 
-    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app...' />
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading activities...' />
 
         return (
             <Grid>
